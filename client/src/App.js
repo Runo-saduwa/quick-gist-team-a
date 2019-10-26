59ae4fe6e4b19d0cf1e231d08b15989a254db845
Joseph Akayesi <josephakayesi@gmail.com>
2019-10-26 00:10:06 +0000

    Implement Chat component

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,12 @@ import { BrowserRouter, Route } from 'react-router-dom'
 
 // Components
 import Join from './components/Join/Join'
+import Join1 from './components/Join/Join1'
 import Chat from './components/Chat/Chat'
 
 const App = () => (
     <BrowserRouter>
-        <Route path='/' exact component={Join} />
+        <Route path='/' exact component={Join1} />
         <Route path='/chat' exact component={Chat} />
     </BrowserRouter>
 )
--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,6 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import queryString from 'query-string'
+import io from 'socket.io-client'
 
-const Chat = () => {
+let socket
+
+const Chat = ({ location }) => {
+    const [name, setName] = useState('')
+    const [room, setRoom] = useState('')
+    const ENDPOINT = 'localhost:5000'
+
+    useEffect(() => {
+        const { name, room } = queryString.parse(location.search)
+        console.log(location.search)
+
+        socket = io(ENDPOINT)
+
+        console.log(socket)
+    })
     return (
         <h1>Chat</h1>
     )
