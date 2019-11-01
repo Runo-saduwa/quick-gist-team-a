67457e9baee41ef316ca3433a779641e51f19178
ogheneruno <orunosaduwa@gmail.com>
2019-11-01 05:41:49 +0100

    added callback function to server.js

--- server/server.js
+++ server/server.js
@@ -12,17 +12,14 @@ const router = require('./routes');
 io.on('connection', (socket) => {
 
 
-  socket.on('join', ({name, room}) => {
+  socket.on('join', ({name, room}, callback) => {
      const {error, user} = addUser({id: socket.id, name, room});
 
-     console.log(name, room)
-
-    //  if(error){
-    //     return callback(error)
-    //  }
+     if(error){
+        return callback(error)
+     }
       
      socket.emit('message', {user:'admin', text:`${user.name}, welcome to the room ${user.room}`})
-      console.log(user.name, user.room)
      socket.broadcast.to(user.room).emit('message', {user:'admin', text: `${user.name}, has joined. Take some time out to say hello!`})
      
      socket.join(user.room);
@@ -31,17 +28,17 @@ io.on('connection', (socket) => {
       
 
      io.to(user.room).emit('roomData', {room:user.room, users:getUsersRoom(user.room)})
-     //callback
+     callback()
 
   });
 
 
-  socket.on('sendMessage', (message) => {
+  socket.on('sendMessage', (message, callback) => {
   const user = getUser(socket.id);
   io.to(user.room).emit('message', {user:user.name, text: message});
   io.to(user.room).emit('roomData', {room:user.room, users: getUsersRoom(user.room)});
 
-  //callback();
+  callback();
   })
 
 
@@ -56,40 +53,7 @@ io.on('connection', (socket) => {
 
 
 // // --- Connection
-// io.on('connection', (socket) => {
 
-//     /*
-//      - * 'join' - listens for join from the client
-//      - *  response expected - this event expects to recieve an object with 'name and room'  in the following format :
-//        * {
-//            name: 'name',
-//            room: 'room
-//          }
-//        *
-//     */
-
-//    socket.on('join', ({name, room}, callback) => {
-//     const {error, user} = addUser({id: socket.id, name, room});
-
-//     if(error){
-//        return callback(error)
-//     }
-     
-//     socket.emit('message', {user:'admin', text:`${user.name}, welcome to the room ${user.room}`})
-//      console.log(user.name, user.room)
-//     socket.broadcast.to(user.room).emit('message', {user:'admin', text: `${user.name}, has joined. Take some time out to say hello!`})
-    
-//     socket.join(user.room);
-
-
-     
-
-//     io.to(user.room).emit('roomData', {room:user.room, users:getUsersRoom(user.room)})
-//     callback();
-
-//    });
-
-   
 //  /*
 //      - * 'sendMessage' - listens for 'sendMessage' event from the client
 //      - *  response expected - this event expects to recieve a string, 'message' in the following format:
@@ -98,26 +62,7 @@ io.on('connection', (socket) => {
 //        *
 //     */
 
-//    socket.on('sendMessage', (message, callback) => {
-//     const user = getUser(socket.id);
-//     io.to(user.room).emit('message', {user:user.name, text: message});
-//     io.to(user.room).emit('roomData', {room:user.room, users: getUsersRoom(user.room)});
 
-//     callback();
-//    })
-
-
-//  socket.on('disconnect', () => {
-//   const user = removeUser(socket.id);
-
-//   if(user){
-//       io.to(user.room).emit('message', {user:'admin', text: `${user.name} has left.`})
-//   }
-
-//  })
-
-
-// })
 
 // Middleware
 app.use(router);
